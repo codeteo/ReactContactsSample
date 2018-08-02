989d2cffdb45d7c2bb5989e7ee1aade842486dc6
codeteo <ramones.teo@gmail.com>
2018-08-02 17:38:57 +0300

    Logs state to console when submit clicked

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -9,13 +9,18 @@ class AddContact extends Component {
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     const { name, email, phone } = this.state;
     return (
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label htmlFor="name">Name</label>
               <input
