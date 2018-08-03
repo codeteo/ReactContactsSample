a1925825c4809373e4206d3b29cd58f79856afec
codeteo <ramones.teo@gmail.com>
2018-08-03 20:30:14 +0300

    Makes request to update contact

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -50,6 +50,21 @@ class EditContact extends Component {
       return;
     }
 
+    const updContact = {
+      name,
+      email,
+      phone
+    };
+
+    const { id } = this.props.match.params;
+
+    const res = await axios.put(
+      `https://jsonplaceholder.typicode.com/users/${id}`,
+      updContact
+    );
+
+    dispatch({ type: "UPDATE-CONTACT", payload: res.data });
+
     // Clear state
     this.setState({
       name: "",
